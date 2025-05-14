8b02dc81e0598ef40169d54f825af5759bd7b038
Joziel Costa <jozielsc@gmail.com>
2025-05-14 18:10:31 -0300

    add ruff

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -1,20 +1,9 @@
 return {
-  "williamboman/mason.nvim",
-  dependencies = {
-    "williamboman/mason-lspconfig.nvim",
-    "neovim/nvim-lspconfig",
-    "hrsh7th/cmp-nvim-lsp",
-  },
+  "neovim/nvim-lspconfig",
+  dependencies = { "hrsh7th/cmp-nvim-lsp" },
   config = function()
-    local mason = require("mason")
-    local mason_lspconfig = require("mason-lspconfig")
     local lspconfig = require("lspconfig")
     local cmp_nvim_lsp = require("cmp_nvim_lsp")
-
-    mason.setup()
-    mason_lspconfig.setup({
-      ensure_installed = { "ruff_lsp", "pyright" }, -- Use "ruff_lsp" para o novo LSP do Ruff
-    })
 
     local capabilities = vim.tbl_deep_extend(
       "force",
@@ -23,24 +12,36 @@ return {
       cmp_nvim_lsp.default_capabilities()
     )
 
-    -- Configuração do Pyright
+    -- Pyright: apenas para IntelliSense (sem análise ou format)
     lspconfig.pyright.setup({
       capabilities = capabilities,
       settings = {
-        pyright = {
-          disableOrganizeImports = true,
-        },
+        pyright = { disableOrganizeImports = true },
         python = {
-          analysis = {
-            ignore = { "*" }, -- Deixe Ruff fazer o lint
-          },
+          analysis = { ignore = { "*" } }, -- ignora análise do Pyright, deixa pro Ruff
         },
       },
     })
 
-    -- Configuração do Ruff LSP
-    lspconfig.ruff_lsp.setup({
+    -- Ruff LSP (substitui ruff_lsp)
+    lspconfig.ruff.setup({
       capabilities = capabilities,
+      on_attach = function(client, bufnr)
+        if client.server_capabilities.documentFormattingProvider then
+          vim.api.nvim_create_autocmd("BufWritePre", {
+            group = vim.api.nvim_create_augroup("RuffFormatOnSave", { clear = true }),
+            buffer = bufnr,
+            callback = function()
+              vim.lsp.buf.format({ bufnr = bufnr })
+            end,
+          })
+        end
+      end,
+      init_options = {
+        settings = {
+          args = {}, -- Coloque flags extras do Ruff aqui se desejar
+        },
+      },
     })
   end,
 }
